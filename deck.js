ebbef6b3494178fab754b02e455313825adc19d8
Roner <Roner@hotmail.se>
2022-05-01 11:01:29 +0200

    Added classes and base for deck.js & index.html

---
+++ deck.js
@@ -0,0 +1,14 @@
+const SUITS = ["♠", "♦", "♣", "♥"]
+
+class Deck {
+    constructor(cards) {
+        this.cards = cards;
+    }
+}
+
+class Card {
+    constructor(suit, value) {
+        this.suit = suit;
+        this.value = value;
+    }
+}
